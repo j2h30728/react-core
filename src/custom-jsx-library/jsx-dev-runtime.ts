d363a8501362807224d5b27364f708c0b98aef2a
Dam <60846068+j2h30728@users.noreply.github.com>
2025-03-11 00:05:37 +0900

    rename: remove unusable file

--- src/custom-jsx-library/jsx-dev-runtime.ts
+++
@@ -1,7 +0,0 @@
-import { jsx } from "./jsx-runtime";
-import { JSXProps } from "./types";
-
-export function jsxDEV(type: string, config: JSXProps, key: string) {
-  return jsx(type, config, key);
-}
-export const Fragment = Symbol.for("jsx.fragment");
