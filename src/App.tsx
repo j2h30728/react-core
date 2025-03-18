6462a39346dad52a4a46d383278b2515c3dd5cc2
Dam <60846068+j2h30728@users.noreply.github.com>
2025-03-18 14:24:37 +0900

    chore: update due to coder reivew

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,42 @@ import { useState } from "custom-jsx-library";
 const App = () => {
   const [todos, setTodos] = useState<{ id: string; value: string; complete: boolean }[]>([]);
 
-  const handleClick = (e: SubmitEvent) => {
+  const handleSubmit = (e: SubmitEvent) => {
     e.preventDefault();
-    const form = e.target as HTMLFormElement;
-    const inputElement = form.querySelector("input") as HTMLInputElement;
+    const formData = new FormData(e.target as HTMLFormElement);
+    const value = formData.get("todoInput")?.toString().trim();
+
+    if (!value) {
+      window.alert("입력을 부탁드립니다.");
+      return;
+    }
+
     const newTodo = {
-      id: Date(),
-      value: inputElement.value,
+      id: Date.now().toString(),
+      value,
       complete: false,
     };
     setTodos((prev) => [...prev, newTodo]);
   };
 
-  const handleToggleComplete = (e: Event) => {
-    e.stopPropagation();
-    const button = e.target as HTMLButtonElement;
-    const selectedId = button.parentElement!.id;
-    setTodos((prev) => prev.map((todo) => (todo.id === selectedId ? { ...todo, complete: !todo.complete } : todo)));
+  const handleToggleComplete = (id: string) => {
+    setTodos((prev) => prev.map((todo) => (todo.id === id ? { ...todo, complete: !todo.complete } : todo)));
   };
 
   return (
     <div>
       <h1>TODO APP</h1>
-      <form onSubmit={handleClick}>
-        <input placeholder="todo" />
+      <form onSubmit={handleSubmit}>
+        <input name="todoInput" placeholder="todo" />
         <button>추가</button>
       </form>
       <ul>
         {todos.map((todo) => (
           <li id={todo.id}>
             <span style={{ textDecorationLine: todo.complete ? "line-through" : "none" }}>{todo.value}</span>
-            <button onClick={handleToggleComplete}>{todo.complete ? "취소" : "완료"}</button>
+            <button type="button" onClick={() => handleToggleComplete(todo.id)}>
+              {todo.complete ? "취소" : "완료"}
+            </button>
           </li>
         ))}
       </ul>
